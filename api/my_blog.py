984ceed031e43eb7429490602048dfd03820cb3a
少 <cell.fantasy@qq.com>
2024-04-25 20:05:44 +0800

    Create my_blog.py

---
+++ api/my_blog.py
@@ -0,0 +1,14 @@
+#-*- coding: utf-8 -*-
+import flask
+from flask import Flask, render_template, request, redirect, url_for
+#from flask_sqlalchemy import SQLAlchemy
+
+app = flask.Flask(__name__)
+@app.route("/")
+
+def getPage():
+    return render_template('shi-shaohua_flask.html', **locals())
+
+if __name__ == '__main__':
+    app.run(debug=True).getPage()
+    print("finished")    
